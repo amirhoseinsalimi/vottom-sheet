0e7d9c4084b0e6e29c5e2848e21173150c834038
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2023-07-04 17:23:47 +0330

    feat: install & configure ESLint + Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,47 +1,59 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+const path = require('node:path');
+const createAliasSetting = require('@vue/eslint-config-airbnb/createAliasSetting');
+
 module.exports = {
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   extends: [
-    '@antfu',
-    'standard',
-    'plugin:vue/base',
+    '@vue/eslint-config-airbnb',
     'eslint:recommended',
+    'plugin:vue/base',
     'plugin:vue/vue3-essential',
     'plugin:vue/vue3-recommended',
     'plugin:vue/vue3-strongly-recommended',
     '@vue/eslint-config-typescript',
-    'plugin:storybook/recommended',
     'prettier',
   ],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     'vue/attribute-hyphenation': [
       'error',
       'never',
       {
-        ignore: []
-      }
+        ignore: [],
+      },
     ],
     'vue/component-tags-order': [
       'error',
       {
-        order: [['template', 'script'], 'style']
-      }
+        order: [['template', 'script'], 'style'],
+      },
     ],
     'vue/custom-event-name-casing': [
       'error',
       'camelCase',
       {
-        ignores: ['/^[a-z]+(?:-[a-z]+)*:[a-z]+$/u']
-      }
-    ]
+        ignores: ['/^[a-z]+(?:-[a-z]+)*:[a-z]+$/u'],
+      },
+    ],
+    'no-return-assign': 0,
+    'import/no-extraneous-dependencies': 0,
+  },
+  settings: {
+    ...createAliasSetting({
+      '@': `${path.resolve(__dirname, './src')}`,
+    }),
   },
   globals: {
     defineProps: 'readonly',
     defineEmits: 'readonly',
+    defineOptions: 'readonly',
     defineExpose: 'readonly',
-    withDefaults: 'readonly'
-  }
-}
+    withDefaults: 'readonly',
+  },
+};
